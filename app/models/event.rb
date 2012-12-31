f3aef6852cc442b3cb3e3f81e3af884cec80994c
Richard Ma <richard.ma.19850509@gmail.com>
2012-12-31 20:37:33 +0800

    update event model (add end_time column). ref #134

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :content, :location, :start_time, :title
+  attr_accessible :content, :location, :start_time, :title, :end_time
 
   validates :title, :start_time, :location, presence: true
 end
