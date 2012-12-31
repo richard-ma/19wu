a44c4ee3e12964a75284c213868b221dc83ba3f7
Richard Ma <richard.ma.19850509@gmail.com>
2012-12-31 20:35:55 +0800

    add end_time to event. ref #134

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121231000051) do
+ActiveRecord::Schema.define(:version => 20121231123510) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20121231000051) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.datetime "end_time"
   end
 
   create_table "users", :force => true do |t|
