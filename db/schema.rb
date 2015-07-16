1e59be92819c1f0ff3b5f439dcf327d6efd9fc6b
Richard Ma <richard.ma.19850509@gmail.com>
2015-07-16 22:48:03 +0800

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
   create_table "event_changes", force: true do |t|
     t.integer  "event_id"
     t.string   "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "event_changes", ["event_id"], name: "index_event_changes_on_event_id", using: :btree
@@ -144,8 +144,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
     t.datetime "end_time"
     t.string   "location"
     t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id",                      null: false
     t.text     "location_guide"
     t.integer  "group_id",                     null: false
@@ -157,8 +157,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
   create_table "fallback_urls", force: true do |t|
     t.string   "origin"
     t.string   "change_to"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "fallback_urls", ["origin"], name: "index_fallback_urls_on_origin", unique: true, using: :btree
@@ -169,8 +169,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
     t.integer  "follower_id",                     null: false
     t.string   "follower_type",                   null: false
     t.boolean  "blocked",         default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   add_index "follows", ["followable_id", "followable_type"], name: "fk_followables", using: :btree
@@ -179,16 +179,16 @@ ActiveRecord::Schema.define(version: 20150516142649) do
   create_table "group_collaborators", force: true do |t|
     t.integer  "group_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "group_topic_replies", force: true do |t|
     t.text     "body"
     t.integer  "group_topic_id", null: false
     t.integer  "user_id",        null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   add_index "group_topic_replies", ["group_topic_id"], name: "index_group_topic_replies_on_group_topic_id", using: :btree
@@ -198,8 +198,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
     t.text     "body"
     t.integer  "user_id",                   null: false
     t.integer  "group_id",                  null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.integer  "replies_count", default: 0
   end
 
@@ -208,8 +208,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
   create_table "groups", force: true do |t|
     t.integer  "user_id",    null: false
     t.string   "slug",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "groups", ["slug"], name: "index_groups_on_slug", unique: true, using: :btree
@@ -217,8 +217,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
   create_table "photos", force: true do |t|
     t.integer  "user_id",    null: false
     t.string   "image"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: true do |t|
@@ -226,8 +226,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
     t.string   "website"
     t.text     "bio"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -259,8 +259,8 @@ ActiveRecord::Schema.define(version: 20150516142649) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "login"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
